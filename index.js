238dfae01ac5aa5e11d2a5d1c9d1d37a74116319
Devang Ajagiya <devangajagiya@gmail.com>
2025-02-10 21:35:39 -0500

    add data

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+const Product = require('./Models/Product.Models.js');
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
+app.use(express.json());
+
+app.post('/api/products', async (req, res) => {
+  try{
+   const product = await Product.create(req.body);
+   res.status(200).json({product});
+  }
+  catch(error){
+    res.status(500).json({message: error.message});
+  }
+});
+
+app.get('/api/products', async (req, res) => {
+  try{
+    const products = await Product.find();
+    res.status(200).json({products});
+  }
+  catch(error){
+    res.status(500).json({message: error.message});
+  }
+});
 
 app.get('/', (req, res) => {
     res.send("Hello World Devang crud operations");
 });
 
-app.post('/API/Products', (req, res) => {
-    console.log(req.body);
-    res.send(Request.body);
-});
+
 
 mongoose.connect("mongodb+srv://devang:Devang63@backend.s7uia.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
   .then(() => {
